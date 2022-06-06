4ff8b68cdd5b09c94b8d722079c23702f68c4ef9
Brian Ryall <brian@brianryall.xyz>
2022-06-06 00:23:59 -0400

    fix: overlay conflict

--- flake.nix
+++ flake.nix
@@ -64,7 +64,23 @@
           packages = flake-utils.lib.filterPackages system (flake-utils.lib.flattenTree nurPkgs);
         }
       ) // rec {
-      overlays = import ./overlays;
+      overlays.default = final: prev:
+        let
+          nurPkgs = removeAttrs (import ./pkgs final prev inputs) [ "callPackage" ];
+
+          pythonOverrides = pyfinal: pyprev:
+            removeAttrs (import ./pkgs/python-modules final pyfinal pyprev) [ "callPackage" ];
+        in
+        nurPkgs // {
+          python3 = prev.python3.override { packageOverrides = pythonOverrides; };
+          python38 = prev.python38.override { packageOverrides = pythonOverrides; };
+          python39 = prev.python39.override { packageOverrides = pythonOverrides; };
+          python310 = prev.python310.override { packageOverrides = pythonOverrides; };
+          python3Packages = final.python3.pkgs;
+          python38Packages = final.python38.pkgs;
+          python39Packages = final.python39.pkgs;
+          python310Packages = final.python310.pkgs;
+        };
       #nixosModules = nixpkgs.lib.mapAttrs (name: value: import value) (import ./modules);
     };
 }
